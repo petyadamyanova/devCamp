130546fdc48e55366fd5f331cd14214d5c9ee344
A <A@A.a>
2023-07-20 21:26:59 +0300

    Initial Commit

---
+++ app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  app
+//
+//  Created by Student on 20.07.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
